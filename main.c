d4b378bd2a9c41d6d5cd170c5aa56da9019c84fc
skn <sk>
2022-08-12 13:19:34 +0900

    day3 comp(output bt 1 line, head and last, with such comment

--- main.c
+++ main.c
@@ -55,41 +55,71 @@ void file_read(char* filename, struct string* head){
 		exit(1);
 	}
 	struct string* current = head;
-	while(fgets(buf, sizeof(buf), fp) != NULL){
+	// only first time
+	if(fgets(buf, sizeof(buf), fp) != NULL){
 		strcpy(current->str, buf);
+	}
+	while(fgets(buf, sizeof(buf), fp) != NULL){
 		insert(current);
 		current = current->next;
+		strcpy(current->str, buf);
 	}
 	fclose(fp);
 	return;
 }
 
+void flag_reset(int *array, int length){
+	for(int i=0;i<length;i++){
+		array[i] = 0;
+	}
+	return;
+}
 		
 int main(int argc, char **argv){
 	struct string* head = (struct string*)malloc(sizeof(struct string));
+	int line_num=1;
 	head->prev = NULL;
 	head->next = NULL;
 	if(argc != 2){
 		printf("usage: ./a.out filename\n");
 	}else{
 		file_read(argv[1], head);
+		struct string* tmp = head;
+		// check line
+		while(tmp->next != NULL){
+			++line_num;
+			tmp = tmp->next;
+		}
+		
 		struct string* current = head;
 		char input_key;
+		int line = 1;
 		if(tcgetattr(STDIN_FILENO, &cookedMode) != 0){
 			perror("tcgetattr() error");
 		}else{
 			cfmakeraw(&rawMode);
 			tcsetattr(STDIN_FILENO, 0, &rawMode);	
-			while(current != NULL){
+			while(1){
 				system("clear");
 				printf("%s", current->str);
-				
+				printf("\n\rline%d\n", line);
+				if(line == 1) printf("\rhead of line\n");
+				else if(line == line_num) printf("\rend of line\n");
+
 				input_key = getchar();
-				if(input_key == 'n'){
-					current = current->next;
-				}
 				if(input_key == 'p'){
+					if(current->prev == NULL){
+						continue;
+					}
+					--line;
 					current = current->prev;
+				}
+				if(input_key == 'n'){
+					if(current->next == NULL){
+						continue;
+					}
+					++line;
+					current = current->next;
 				}
 				if(input_key == 'e'){
 					system("clear");
@@ -102,3 +132,4 @@ int main(int argc, char **argv){
 	}
 	return 0;
 }
+
