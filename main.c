9dbb2ab931866fb60ada93cc37aa09185909aa2c
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2022-08-15 09:34:36 +0900

    下にスクロールのみ

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
+#include <string.h>
 #include <unistd.h>
 #include <termios.h>
 #include "str.h"
@@ -12,6 +12,7 @@ struct termios cookedMode, rawMode;
 int main(int argc, char **argv){
 	struct str* head = (struct str*)malloc(sizeof(struct str));
 	int line_num=1;
+	int cursor[2] = {1, 1};
 	head->prev = NULL;
 	head->next = NULL;
 	if(argc != 2){
@@ -28,32 +29,70 @@ int main(int argc, char **argv){
 		struct str* current = head;
 		char input_key;
 		int line = 1;
+		int line_limit = 40;
 		if(tcgetattr(STDIN_FILENO, &cookedMode) != 0){
 			perror("tcgetattr() error");
 		}else{
 			cfmakeraw(&rawMode);
-			tcsetattr(STDIN_FILENO, 0, &rawMode);	
+			tcsetattr(STDIN_FILENO, 0, &rawMode);
+			system("clear");
+			if(line_num<40) line_limit = line_num;
+			for(int i=0;i<line_limit;i++){
+				//system("clear");
+				printf("%s\r", current->str);
+				current = current->next;
+			}
+			// back 1 line
+			printf("\033[1;1H");
+			current = head;
+			cursor[0]= 1;
+			cursor[1]= 1;
+			//printf("\033[%02d;%02dH", 1, 1);
+			int offset;
 			while(1){
 				system("clear");
-				printf("%s", current->str);
-				printf("\n\rline%d\n", line);
-				if(line == 1) printf("\rhead of line\n");
-				else if(line == line_num) printf("\rend of line\n");
-
+				for(int i=0;i<line_limit;i++){
+					printf("%s\r", current->str);
+					current = current->next;
+				}
+				printf("\033[41;1H");
+				printf("\033[0J");
+				printf("line%d", line);
+				printf("\033[%d;1H", cursor[0]);
 				input_key = getchar();
-				if(input_key == 'p'){
+				if(input_key == 'k'){
 					if(current->prev == NULL){
 						continue;
 					}
 					--line;
+					cursor[0] = line;
 					current = current->prev;
+					printf("\033[1A");
 				}
-				if(input_key == 'n'){
-					if(current->next == NULL){
+				if(input_key == 'j'){
+					if(current == NULL){
+						current = head;
+						for(int i=0;i<offset;i++){
+							current=current->next;
+						}
 						continue;
 					}
 					++line;
 					current = current->next;
+					if(line - line_limit <= 0){
+						offset = 0;
+					}else{
+						offset = line - line_limit;
+					}
+					current = head;
+					for(int i=0;i<offset;i++){
+						current=current->next;
+					}
+					if(!(line<=line_num && offset > 0)){
+						// cursor is more than 40 line
+						//current = head;
+						cursor[0] += 1;
+					}
 				}
 				if(input_key == 'e'){
 					system("clear");
